0076b1b6bd5b562c55793043fe3a7defcc85531e
igorshinal <igorshinal@gmail.com>
2018-01-12 22:46:13 +0200

    cat component

---
+++ src/components/Cat.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div class="md-layout md-gutter md-alignment-center">
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Cat"
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import "~vue-material/src/components/MdAnimation/variables";
+  @import "~vue-material/src/theme/engine";
+
+  .md-layout-item {
+    height: 40px;
+    margin-top: 8px;
+    margin-bottom: 8px;
+    transition: .3s $md-transition-stand-timing;
+
+  &:after {
+     width: 100%;
+     height: 100%;
+     display: block;
+     background: md-get-palette-color(purple, 200);
+     content: " ";
+   }
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+import VueRouter from 'vue-router'
+
+
+Vue.use({ VueMaterial })
+Vue.use(VueRouter)
+
 
 new Vue({
   el: '#app',
